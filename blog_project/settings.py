1c43ca520a850ebbf0bd88e7dbf22c0c8c360e8c
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2020-07-11 17:26:32 -0500

    add heroku configs

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'syyhgn@tev!xil@420i4y$=&zu+x^+($z@##1bp_28pw@a!n%k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
